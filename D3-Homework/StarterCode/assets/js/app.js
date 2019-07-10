ec14a322454318e9beb3b9724a865a501473a4df
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-10 10:57:39 -0500

    create nodemon dependencies file

--- D3-Homework/StarterCode/assets/js/app.js
+++ D3-Homework/StarterCode/assets/js/app.js
@@ -1,8 +1,11 @@
 // @TODO: YOUR CODE HERE!
+
 console.log("hello!");
+/*
 d3.csv("assets/data.csv", function(error, healthData) {
     if (error) return console.warn(error);
 
     console.log(healthData)
 
 });
+*/
