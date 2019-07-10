38607c13213cf468f22b1fcd6016a735c30a8763
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-10 16:07:30 -0500

    add opacity to text

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -8,3 +8,31 @@ d3.select("svg")
     .attr("y2",400)
     .style("stroke","black")
     .style("stroke-width","2px")
+
+d3.select("svg")
+    .append("text")
+    .attr("x",20)
+    .attr("y",20)
+    .text("HELLO");
+
+ d3.select("svg")
+    .append("circle")
+    .attr("r",20)
+    .attr("cx",20)
+    .attr("cy",20)
+    .style("fill","red");
+
+    d3.select("svg")
+    .append("circle")
+    .attr("r",50)
+    .attr("cx",130)
+    .attr("cy",100)
+    .style("fill","lightblue");
+
+d3.select("svg")
+    .append("text")
+    .attr("x",130)
+    .attr("y",100)
+    .text("WORLD")
+    .style("opacity",0.3)
+    .text("Uh,...hi")
