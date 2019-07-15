031abeda5439ec6f7cbd6049e63ffae8a0e68214
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-14 22:10:38 -0500

    bind data to svg and create rectangles

--- D3-Homework/assets/public/js/app.js
+++ D3-Homework/assets/public/js/app.js
@@ -5,17 +5,27 @@ console.log("hello!");
 d3.select("body")
 
 //upload data from csv file
-//d3.csv("data/data.csv",  healthData=> {
+d3.csv("data/data.csv",  healthData=> {
 
   //add svg
-  /*
+  
   d3.select(".chart")
     .append("svg")
-    .select("rect")
+    .attr("height",900)
+    .attr("width","100%")
+
+  d3.select("svg")
+    .selectAll("rect")
     .data(healthData)
     .enter()
     .append("rect")
-*/
+    .attr("width",50)
+    .attr("height", d => d.poverty*10)
+    .attr("x", d=> d.poverty+20)
+    .attr("y",10)
+    .attr("fill","blue")
+
+
   //bind the data to the chart
 
 
@@ -25,12 +35,12 @@ d3.select("body")
   //format the data
 
   //set up the scales
- // console.log(healthData);
+ console.log(healthData);
 
 
   
 
 
-//});
+});
 
 
