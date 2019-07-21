1835d2cbbf9d38f0dfe392b7ebd62cd2e21d5d32
Zaynaib Giwa <zaynaibg@gmail.com>
2019-07-21 14:41:29 -0500

    completed homework scatter plot

--- D3-Homework/assets/public/js/app.js
+++ D3-Homework/assets/public/js/app.js
@@ -2,43 +2,78 @@
 
 console.log("hello!");
 
-d3.select("body")
+//SET UP for svg wrapper
+let svgWidth = 960;
+let svgHeight = 600;
+
+const margin = {top:50, right:50,bottom:50,left:50};
+
+let chartWidth = svgWidth - margin.right - margin.left;
+let chartHeight = svgHeight - margin.top - margin.bottom;
+
+//create a svg wrapper to hold the chart and shift graph for margins
+
+let svg = d3.select(".chart")
+            .append("svg")
+            .attr("width",svgWidth)
+            .attr("height",svgHeight)
+            .append("g")
+            .attr("transform",`translate(${margin.left},${margin.top})`)
+
+let chart = svg.append("g");
 
 //upload data from csv file
 d3.csv("data/data.csv",  healthData=> {
+  //if (err) throw err;
+  healthData.forEach((data) =>{
+    data.poverty = Number(data.poverty);
+    data.healthcare = +data.healthcare;
+  })
 
-  //add svg
+  //scale functions for axis
+  let yLinearScale = d3.scaleLinear()
+                        .range([chartHeight,0]) //how big your outputs are
+                        .domain([0, d3.max(healthData,data => +data.healthcare) * 1.2])
   
-  d3.select(".chart")
-    .append("svg")
-    .attr("height",900)
-    .attr("width","100%")
-
-  d3.select("svg")
-    .selectAll("rect")
-    .data(healthData)
-    .enter()
-    .append("rect")
-    .attr("width",50)
-    .attr("height", d => d.poverty*10)
-    .attr("x", d=> d.poverty+20)
-    .attr("y",10)
-    .attr("fill","blue")
+  let xLinearScale = d3.scaleLinear()
+                      .range([0,chartWidth])
+                      .domain([d3.min(healthData, data => +data.poverty) - 1, d3.max(healthData, data => +data.poverty) + 1]);
 
 
-  //bind the data to the chart
+  let bottomAxis = d3.axisBottom()
+                      .scale(xLinearScale);
+  
+  let leftAxis = d3.axisLeft()
+                    .scale(yLinearScale);
+  
 
 
-  //set up the svg width/height/margins
+
+//append the dots to the graph
+chart.selectAll("circle")
+      .data(healthData)
+      .enter().append("circle")
+      .attr("cx",(data,index)=>xLinearScale(data.poverty))
+      .attr("cy",(data,index)=>yLinearScale(data.healthcare))
+      .attr("r",15)
+      .attr("fill","pink")
 
 
-  //format the data
+//append the axis
+
+chart.append("g")
+    .attr('transform',`translate(0,${chartHeight})`)
+    .call(bottomAxis)
+  chart.append("g")
+  //leftAxis(g);
+        .call(leftAxis)
+
+  chart.selectAll("circle").call(function(selection) {
+    console.log(selection);
+  });
 
   //set up the scales
- console.log(healthData);
-
-
-  
+ //console.log(healthData);
 
 
 });
