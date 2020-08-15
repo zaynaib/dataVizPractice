c3662d8db1db4bf036446ad848224525e4337793
Zaynaib Giwa <zaynaibg@gmail.com>
2020-08-14 23:56:11 -0500

    force graph working

--- Day7/beeswarm.js
+++ Day7/beeswarm.js
@@ -12,7 +12,7 @@ beeswarm = async () =>{
     width: window.innerWidth * 0.9,
     height: 400,
     margin:{
-        top:15,
+        top:25,
         right:15,
         bottom:40,
         left:60,
@@ -34,28 +34,68 @@ const bounds = wrapper.append("g")
 
 //make my scales
 //
+const nodes = dataset.map(function(d, i) {
+  return {
+    value: +d.recalc,
+    name: d.rapper_clean,
+    radius: 5
+  }
+});
+
+/*
+index - the node’s zero-based index into nodes
+x - the node’s current x-position
+y - the node’s current y-position
+vx - the node’s current x-velocity
+vy - the node’s current y-velocity
+
+*/
+
+console.log(nodes)
 const xScale = d3.scaleLinear()
                  .domain(d3.extent(dataset,xAcessor))
                  .range([0,dimensions.boundedWidth])
                  .nice()
+                 
+                 var simulation = d3.forceSimulation(nodes)
+                 .force('charge', d3.forceManyBody().strength(5))
+                 .force('x', d3.forceX().x(function(d) {
+                   return xScale(d.value);
+                 }))
+                 .force('y', d3.forceY().y(function(d) {
+                   return 0;
+                 }))
+                 .force('collision', d3.forceCollide().radius(function(d) {
+                   return d.radius;
+                 })).on('tick', ticked);
 
-console.log(xScale(2998))
 
-const yScale = d3.scaleLinear()
-                 .domain(d3.extent(dataset,xAcessor))
-                 .range([dimensions.boundedHeight,0])
-           
 
-console.log('apple')
-console.log(yScale(2998))
-const circle = bounds.append("g").selectAll('circle')
-              .data(dataset)
-              .join("circle")
-              .attr("cx",d=>xScale(xAcessor(d)))
-              .attr("cy", 35)
-              .attr("r",5)
-              .attr("fill","blue")
+                 function ticked() {
+                  var u = d3.select('svg g')
+                    .selectAll('circle')
+                    .data(nodes)
+                
+                  u.enter()
+                    .append('circle')
+                    .attr('r', function(d) {
+                      return d.radius;
+                    })
+                    .style('fill', function(d) {
+                      return "blue";
+                    })
+                    .merge(u)
+                    .attr('cx', function(d) {
+                      return d.x;
+                    })
+                    .attr('cy', function(d) {
+                      return d.y + 50;
+                    })
+                
+                  u.exit().remove();
+                }
 
+     
 }
 
 beeswarm();
