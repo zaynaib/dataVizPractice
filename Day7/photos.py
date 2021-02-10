50e0704c9d4b522de5f38d513bc9fab179ae98f9
Zaynaib Giwa <zaynaibg@gmail.com>
2021-02-10 12:37:07 -0600

    downloaded selenium for web scrapping

---
+++ Day7/photos.py
@@ -0,0 +1,20 @@
+
+#beautiful soup
+#seleium
+#google driver
+#read in csv
+
+#open the file use with statement
+
+#get rappers name by id
+
+
+#call to flickr api 
+#grab photo
+
+#first open the browser
+#select the first image in result
+#download to my folder
+
+#pip install selenuim
+#download web driver
